4303b0fabd86c76635c8e7565462140b6bef8532
Shubham <shubhamkumarwinner@gmail.com>
2021-07-14 11:49:05 +0530

    Migrating to navigation

--- app/src/main/java/com/shubhamkumarwinner/composenavigation/RallyActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composenavigation/RallyActivity.kt
@@ -6,13 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.shubhamkumarwinner.composenavigation.RallyScreen.*
+import com.shubhamkumarwinner.composenavigation.data.Bill
+import com.shubhamkumarwinner.composenavigation.data.UserData
+import com.shubhamkumarwinner.composenavigation.ui.accounts.AccountsBody
+import com.shubhamkumarwinner.composenavigation.ui.bills.BillsBody
 import com.shubhamkumarwinner.composenavigation.ui.components.RallyTabRow
+import com.shubhamkumarwinner.composenavigation.ui.overview.OverviewBody
 import com.shubhamkumarwinner.composenavigation.ui.theme.RallyTheme
 
 class RallyActivity : ComponentActivity() {
@@ -27,26 +38,36 @@ class RallyActivity : ComponentActivity() {
 @Composable
 fun RallyApp() {
     RallyTheme {
-        val allScreens = RallyScreen.values().toList()
-        var  currentScreen by rememberSaveable{
-            mutableStateOf(RallyScreen.Overview)
-
-        }
+        val allScreens = values().toList()
+        val navController = rememberNavController()
+        val backStackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = RallyScreen.fromRoute(
+            backStackEntry.value?.destination?.route
+        )
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { screen -> navController.navigate(screen.name) },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.content(
-                    onScreenChange = { screen ->
-                        currentScreen = RallyScreen.valueOf(screen)
-                    }
-                )
+            NavHost(navController = navController,
+                startDestination = Overview.name,
+                modifier = Modifier.padding(innerPadding)) {
+                composable(Overview.name){
+                    OverviewBody(
+                        onClickSeeAllAccounts = {navController.navigate(Accounts.name)},
+                        onClickSeeAllBills = {navController.navigate(Bills.name)}
+                    )
+                }
+                composable(Bills.name){
+                    BillsBody(bills = UserData.bills)
+                }
+                composable(Accounts.name){
+                    AccountsBody(accounts = UserData.accounts)
+                }
             }
         }
     }
--- app/src/main/java/com/shubhamkumarwinner/composenavigation/RallyScreen.kt
+++ app/src/main/java/com/shubhamkumarwinner/composenavigation/RallyScreen.kt
@@ -13,25 +13,16 @@ import com.shubhamkumarwinner.composenavigation.ui.overview.OverviewBody
 
 enum class RallyScreen(
     val icon: ImageVector,
-    val body: @Composable ((String) -> Unit) -> Unit
 ) {
     Overview(
         icon = Icons.Filled.PieChart,
-        body = { OverviewBody() }
     ),
     Accounts(
         icon = Icons.Filled.AttachMoney,
-        body = { AccountsBody(accounts = UserData.accounts) }
     ),
     Bills(
         icon = Icons.Filled.MoneyOff,
-        body = { BillsBody(UserData.bills) }
     );
-
-    @Composable
-    fun content(onScreenChange: (String) -> Unit) {
-        body(onScreenChange)
-    }
 
     companion object {
         fun fromRoute(route: String?): RallyScreen =
